f6df5347db234187bea00bd54043892ce993464e
Jan-Dro <jandrocodes@icloud.com>
2023-07-28 22:16:55 -0500

    trying

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+// const cards = document.querySelectorAll('.card');
+
+// cards.forEach(card => {
+//   card.addEventListener('mouseover', () => {
+//     card.classList.add('expanded');
+//   });
+
+//   card.addEventListener('mouseout', () => {
+//     card.classList.remove('expanded');
+//   });
+// });
 const cards = document.querySelectorAll('.card');
 
-cards.forEach(card => {
-  card.addEventListener('mouseover', () => {
-    card.classList.add('expanded');
-  });
+// Function to handle the click and touch events
+function handleCardInteraction(card) {
+  card.classList.toggle('expanded');
+}
 
-  card.addEventListener('mouseout', () => {
-    card.classList.remove('expanded');
-  });
+// Add click and touch event listeners to each stack card
+cards.forEach(card => {
+  card.addEventListener('click', () => handleCardInteraction(card));
+  card.addEventListener('touchstart', () => handleCardInteraction(card));
 });
