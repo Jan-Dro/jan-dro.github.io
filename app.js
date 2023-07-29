575835f3f283189a1b0ef803f104960737f1c11a
Jan-Dro <jandrocodes@icloud.com>
2023-07-28 20:01:11 -0500

    fixing touch

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+const cards = document.querySelectorAll('.card');
 
-  const cards = document.querySelectorAll('.card');
+cards.forEach(card => {
+  card.addEventListener('mouseover', () => {
+    card.classList.add('expanded');
+  });
 
-  cards.forEach(card => {
-    card.addEventListener('mouseover', () => {
-      card.classList.add('expanded');
-    });
+  card.addEventListener('mouseout', () => {
+    card.classList.remove('expanded');
+  });
 
-    card.addEventListener('mouseout', () => {
-      card.classList.remove('expanded');
+  card.addEventListener('touchstart', () => {
+    card.classList.toggle('expanded');
+
+    cards.forEach(c => {
+      if (c !== card && c.classList.contains('expanded')) {
+        c.classList.remove('expanded');
+      }
     });
   });
+});
