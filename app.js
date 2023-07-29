ea097a9b1871578b625e5667f0097b12d7c96e87
Jan-Dro <jandrocodes@icloud.com>
2023-07-28 20:51:19 -0500

    revised

--- app.js
+++ app.js
@@ -8,14 +8,4 @@ cards.forEach(card => {
   card.addEventListener('mouseout', () => {
     card.classList.remove('expanded');
   });
-
-  card.addEventListener('touchstart', () => {
-    card.classList.toggle('expanded');
-
-    cards.forEach(c => {
-      if (c !== card && c.classList.contains('expanded')) {
-        c.classList.remove('expanded');
-      }
-    });
-  });
 });
