096d190078898aee9c2e404c8adf00983be51bb1
Jandro <126230256+Jan-Dro@users.noreply.github.com>
2023-09-16 14:02:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-// const cards = document.querySelectorAll('.card');
-
-// cards.forEach(card => {
-//   card.addEventListener('mouseover', () => {
-//     card.classList.add('expanded');
-//   });
-
-//   card.addEventListener('mouseout', () => {
-//     card.classList.remove('expanded');
-//   });
-// });
 const cards = document.querySelectorAll('.card');
 let touchStartTime, touchEndTime;
 
