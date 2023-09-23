eb89b7b1e4d8ba237ab0f2b8f774dfe8ae2f1cb1
Jandro <126230256+Jan-Dro@users.noreply.github.com>
2023-09-23 18:11:27 -0500

    Update card.jsx

--- portfolio/src/card.jsx
+++ portfolio/src/card.jsx
@@ -2,29 +2,37 @@ import React, { useState } from 'react';
 
 function Card({ title, content }) {
   const [expanded, setExpanded] = useState(false);
+  const [touchStartTime, setTouchStartTime] = useState(null);
 
   const handleCardInteraction = () => {
     setExpanded(!expanded);
   };
 
   const onTouchStart = () => {
-    const touchStartTime = new Date().getTime();
-    handleCardInteraction();
+    setTouchStartTime(new Date().getTime());
   };
 
   const onTouchEnd = () => {
     const touchEndTime = new Date().getTime();
     const touchDuration = touchEndTime - touchStartTime;
+
     if (touchDuration < 300) {
+      handleCardInteraction();
     }
   };
 
   return (
-    <div className={`card ${expanded ? 'expanded' : ''}`} onClick={handleCardInteraction} onTouchStart={onTouchStart} onTouchEnd={onTouchEnd}>
+    <div
+      className={`card ${expanded ? 'expanded' : ''}`}
+      onClick={handleCardInteraction}
+      onTouchStart={onTouchStart}
+      onTouchEnd={onTouchEnd}
+    >
       <h3 className="title">{title}</h3>
       <ul className="content">{content.map((item, index) => <li key={index}>{item}</li>)}</ul>
     </div>
   );
 }
 
+export default Card;
 export default Card;
